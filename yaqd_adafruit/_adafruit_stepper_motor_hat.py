f493d743180b912b58557fb2204c8ce7fc22fad1
Kyle Sunden <git@ksunden.space>
2022-02-07 17:34:36 -0600

    Switch to internal pin, fix homing behavior, add unit scalar

--- yaqd_adafruit/_adafruit_stepper_motor_hat.py
+++ yaqd_adafruit/_adafruit_stepper_motor_hat.py
@@ -1,7 +1,7 @@
 import asyncio
 from typing import Dict, Any
 
-import yaqc  # type: ignore
+import gpiozero  # type: ignore
 from yaqd_core import UsesI2C, UsesSerial, HasLimits, HasPosition, IsHomeable, IsDaemon
 
 
@@ -15,57 +15,61 @@ class AdafruitStepperMotorHat(UsesI2C, UsesSerial, IsHomeable, HasLimits, HasPos
         super().__init__(name, config, config_filepath)
         self._kit = MotorKit(address=config["i2c_addr"], steppers_microsteps=config["microsteps"])
         self._stepper = getattr(self._kit, f"stepper{config['stepper_index']}")
-        self._homing = False
-        self._lower_client = None
-        self._upper_client = None
+        self.steps_per_unit = config["steps_per_unit"]
+        self._units = config["units"]
+        self.microsteps = config["microsteps"]
+        self._lock = asyncio.Lock()
+        self._lower_pin = gpiozero.InputDevice(config["lower_limit_switch"]["pin"], pull_up=True)
+        if config.get("upper_limit_switch"):
+            self._upper_pin = gpiozero.InputDevice(
+                config["upper_limit_switch"]["pin"], pull_up=True
+            )
 
     async def _do_step(self, backward=False):
         from adafruit_motor import stepper  # type: ignore
+
+        steps = self.to_steps(self._state["position"])
         direction = stepper.BACKWARD if backward else stepper.FORWARD
         if direction == stepper.BACKWARD and await self._get_lower_limit_switch():
             return
         elif direction == stepper.FORWARD and await self._get_upper_limit_switch():
             return
         self._stepper.onestep(direction=direction, style=stepper.MICROSTEP)
-        self._state["position"] += 1 if direction == stepper.FORWARD else -1
+        steps += 1 if direction == stepper.FORWARD else -1
+        self._state["position"] = self.to_units(steps)
+        self.logger.debug(f"{self._state['position']}")
 
+    def to_units(self, usteps):
+        return usteps / self.steps_per_unit / self.microsteps
+
+    def to_steps(self, units):
+        return round(units * self.steps_per_unit * self.microsteps)
 
     async def _get_lower_limit_switch(self):
-        if not self._lower_client:
-            port = self._config["lower_limit_switch"]["port"]
-            self._lower_client = yaqc.Client(port)
-        self._lower_client.measure()
-        while self._lower_client.busy():
-            await asyncio.sleep(0)
         # TODO: invert
-        return self._lower_client.get_measured()[self._config["lower_limit_switch"]["channel"]]
+        return self._lower_pin.value
 
     async def _get_upper_limit_switch(self):
         if not self._config.get("upper_limit_switch"):
             return False
-        if not self._upper_client:
-            port = self._config["upper_limit_switch"]["port"]
-            self._upper_client = yaqc.Client(port)
-        self._upper_client.measure()
-        while self._upper_client.busy():
-            await asyncio.sleep(0)
         # TODO: invert
-        return self._upper_client.get_measured()[self._config["upper_limit_switch"]["channel"]]
+        return self._upper_pin.value
 
     def home(self):
         self._busy = True
         self._loop.create_task(self._home())
 
     async def _home(self):
-        self._homing = True
         prev_position = self._state["position"]
-        while True:
-            await self._do_step(backward=True)
-            if self._state["position"] == prev_position:
-                break
-            prev_position = self._state["position"]
-        self._state["position"] = 0
-        self._homing = False
+        async with self._lock:
+            self._busy = True
+            while True:
+                await asyncio.sleep(0)
+                await self._do_step(backward=True)
+                if self._state["position"] == prev_position:
+                    break
+                prev_position = self._state["position"]
+            self._state["position"] = 0
 
     def _set_position(self, position):
         # destination gets set by parent
@@ -74,14 +78,22 @@ class AdafruitStepperMotorHat(UsesI2C, UsesSerial, IsHomeable, HasLimits, HasPos
 
     async def update_state(self):
         while True:
-            while self._state["position"] != self._state["destination"]:
-                if self._homing:
-                    await asyncio.sleep(1)
-                    continue
-                self._busy = True
-                await self._do_step(self._state["position"] > self._state["destination"])
-            await asyncio.sleep(0.01)
-            #await self._busy_sig.wait()
+            async with self._lock:
+                while self.to_steps(self._state["position"]) != self.to_steps(
+                    self._state["destination"]
+                ):
+                    self._busy = True
+                    await asyncio.sleep(0)
+                    await self._do_step(
+                        self.to_steps(self._state["position"])
+                        > self.to_steps(self._state["destination"])
+                    )
+
+                self._busy = False
+            await self._busy_sig.wait()
 
     def direct_serial_write(self, msg):
         pass
+
+    def close(self):
+        self._stepper.release()
