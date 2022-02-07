cdabded89fa80c699045ac724024fdb7a44c3cb7
Kyle Sunden <git@ksunden.space>
2022-02-07 17:33:48 -0600

    Make daemon run

--- yaqd_adafruit/_adafruit_stepper_motor_hat.py
+++ yaqd_adafruit/_adafruit_stepper_motor_hat.py
@@ -10,20 +10,25 @@ class AdafruitStepperMotorHat(UsesI2C, UsesSerial, IsHomeable, HasLimits, HasPos
 
     def __init__(self, name, config, config_filepath):
         # adafruit_motorkit raises an exception if not rpi
-        from adafruit_motorkit import stepper, MotorKit  # type: ignore
+        from adafruit_motorkit import MotorKit  # type: ignore
 
         super().__init__(name, config, config_filepath)
         self._kit = MotorKit(address=config["i2c_addr"], steppers_microsteps=config["microsteps"])
-        self._stepper = getattr(self.kit, f"stepper{config['stepper_index']}")
+        self._stepper = getattr(self._kit, f"stepper{config['stepper_index']}")
         self._homing = False
+        self._lower_client = None
+        self._upper_client = None
 
     async def _do_step(self, backward=False):
+        from adafruit_motor import stepper  # type: ignore
         direction = stepper.BACKWARD if backward else stepper.FORWARD
         if direction == stepper.BACKWARD and await self._get_lower_limit_switch():
             return
         elif direction == stepper.FORWARD and await self._get_upper_limit_switch():
             return
-        self.stepper.onestep(direction=direction, style=stepper.MICROSTEP)
+        self._stepper.onestep(direction=direction, style=stepper.MICROSTEP)
+        self._state["position"] += 1 if direction == stepper.FORWARD else -1
+
 
     async def _get_lower_limit_switch(self):
         if not self._lower_client:
@@ -36,7 +41,7 @@ class AdafruitStepperMotorHat(UsesI2C, UsesSerial, IsHomeable, HasLimits, HasPos
         return self._lower_client.get_measured()[self._config["lower_limit_switch"]["channel"]]
 
     async def _get_upper_limit_switch(self):
-        if not self._config["upper_limit_switch"]:
+        if not self._config.get("upper_limit_switch"):
             return False
         if not self._upper_client:
             port = self._config["upper_limit_switch"]["port"]
@@ -53,10 +58,13 @@ class AdafruitStepperMotorHat(UsesI2C, UsesSerial, IsHomeable, HasLimits, HasPos
 
     async def _home(self):
         self._homing = True
-        while not self.get_lower_limit_switch():
-            self.do_step(backward=True)
-            await asyncio.sleep(0)
-        self._state["positon"] = 0
+        prev_position = self._state["position"]
+        while True:
+            await self._do_step(backward=True)
+            if self._state["position"] == prev_position:
+                break
+            prev_position = self._state["position"]
+        self._state["position"] = 0
         self._homing = False
 
     def _set_position(self, position):
@@ -68,9 +76,12 @@ class AdafruitStepperMotorHat(UsesI2C, UsesSerial, IsHomeable, HasLimits, HasPos
         while True:
             while self._state["position"] != self._state["destination"]:
                 if self._homing:
-                    await asyncio.sleep(0.01)
+                    await asyncio.sleep(1)
                     continue
                 self._busy = True
-                await self._do_step(self._state["position"] < self._state["destination"])
-                await asyncio.sleep(0)
-            await self._busy_sig.wait()
+                await self._do_step(self._state["position"] > self._state["destination"])
+            await asyncio.sleep(0.01)
+            #await self._busy_sig.wait()
+
+    def direct_serial_write(self, msg):
+        pass
