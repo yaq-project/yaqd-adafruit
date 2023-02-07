c062d5e93c44276427f40696dcc684ad6084d577
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2023-02-07 09:46:31 -0600

    handle microsteps=1

--- yaqd_adafruit/_adafruit_stepper_motor_hat.py
+++ yaqd_adafruit/_adafruit_stepper_motor_hat.py
@@ -3,6 +3,7 @@ from typing import Dict, Any
 
 import gpiozero  # type: ignore
 from yaqd_core import UsesI2C, UsesSerial, HasLimits, HasPosition, IsHomeable, IsDaemon
+from adafruit_motor import stepper  # type: ignore
 
 
 class AdafruitStepperMotorHat(UsesI2C, UsesSerial, IsHomeable, HasLimits, HasPosition, IsDaemon):
@@ -13,11 +14,15 @@ class AdafruitStepperMotorHat(UsesI2C, UsesSerial, IsHomeable, HasLimits, HasPos
         from adafruit_motorkit import MotorKit  # type: ignore
 
         super().__init__(name, config, config_filepath)
-        self._kit = MotorKit(address=config["i2c_addr"], steppers_microsteps=config["microsteps"])
+        self.microsteps = config["microsteps"]
+        self.style = stepper.MICROSTEP if self.microsteps > 1 else stepper.SINGLE
+        self._kit = MotorKit(
+            address=config["i2c_addr"],
+            steppers_microsteps=self.microsteps if self.microsteps > 1 else None
+        )
         self._stepper = getattr(self._kit, f"stepper{config['stepper_index']}")
         self.steps_per_unit = config["steps_per_unit"]
         self._units = config["units"]
-        self.microsteps = config["microsteps"]
         self._lock = asyncio.Lock()
         self._lower_pin = gpiozero.InputDevice(config["lower_limit_switch"]["pin"], pull_up=True)
         if config.get("upper_limit_switch"):
@@ -26,7 +31,6 @@ class AdafruitStepperMotorHat(UsesI2C, UsesSerial, IsHomeable, HasLimits, HasPos
             )
 
     async def _do_step(self, backward=False):
-        from adafruit_motor import stepper  # type: ignore
 
         steps = self.to_steps(self._state["position"])
         direction = stepper.BACKWARD if backward else stepper.FORWARD
@@ -34,7 +38,7 @@ class AdafruitStepperMotorHat(UsesI2C, UsesSerial, IsHomeable, HasLimits, HasPos
             return
         elif direction == stepper.FORWARD and await self._get_upper_limit_switch():
             return
-        self._stepper.onestep(direction=direction, style=stepper.MICROSTEP)
+        self._stepper.onestep(direction=direction, style=self.style)
         steps += 1 if direction == stepper.FORWARD else -1
         self._state["position"] = self.to_units(steps)
         self.logger.debug(f"{self._state['position']}")
