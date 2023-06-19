ee03ca7ac2ffce1f56a351350064e2e3518537c1
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2023-06-19 12:02:25 -0500

    Update adafruit-stepper-motor-hat.toml (#16)
    * Update adafruit-stepper-motor-hat.toml
    
    * allow for no microsteps (UNTESTED atm)
    * docstring clarifies allowed values of microsteps
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * handle microsteps=1
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update _adafruit_stepper_motor_hat.py
    
    double gives more torque, but also more vibration
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * debug rpi
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * step style controls step size
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update _adafruit_stepper_motor_hat.py
    
    * correct style symbols
    
    * correct style types
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update _adafruit_stepper_motor_hat.py
    
    * remove unnecessary error handles
    
    * refine movement
    
    remove async from sync methods
    position moving stops when within step size of destination
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update _adafruit_stepper_motor_hat.py
    
    * remove release message
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update _adafruit_stepper_motor_hat.py
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- yaqd_adafruit/_adafruit_stepper_motor_hat.py
+++ yaqd_adafruit/_adafruit_stepper_motor_hat.py
@@ -3,6 +3,15 @@ from typing import Dict, Any
 
 import gpiozero  # type: ignore
 from yaqd_core import UsesI2C, UsesSerial, HasLimits, HasPosition, IsHomeable, IsDaemon
+from adafruit_motor import stepper  # type: ignore
+
+
+styles = {
+    "DOUBLE": stepper.DOUBLE,
+    "SINGLE": stepper.SINGLE,
+    "INTERLEAVE": stepper.INTERLEAVE,
+    "MICROSTEP": stepper.MICROSTEP,
+}
 
 
 class AdafruitStepperMotorHat(UsesI2C, UsesSerial, IsHomeable, HasLimits, HasPosition, IsDaemon):
@@ -13,11 +22,21 @@ class AdafruitStepperMotorHat(UsesI2C, UsesSerial, IsHomeable, HasLimits, HasPos
         from adafruit_motorkit import MotorKit  # type: ignore
 
         super().__init__(name, config, config_filepath)
-        self._kit = MotorKit(address=config["i2c_addr"], steppers_microsteps=config["microsteps"])
+        self.microsteps = config["microsteps"]
+        self._kit = MotorKit(
+            address=config["i2c_addr"],
+            steppers_microsteps=self.microsteps,
+        )
         self._stepper = getattr(self._kit, f"stepper{config['stepper_index']}")
+        if config["style"] in ["DOUBLE", "SINGLE"]:  # full steps only
+            self.step_size = self.microsteps
+        elif config["style"] == "INTERLEAVE":  # half stepping
+            self.step_size = self.microsteps // 2
+        elif config["style"] == "MICROSTEP":  # microstepping
+            self.step_size = 1
+        self.style = styles[config["style"]]
         self.steps_per_unit = config["steps_per_unit"]
         self._units = config["units"]
-        self.microsteps = config["microsteps"]
         self._lock = asyncio.Lock()
         self._lower_pin = gpiozero.InputDevice(config["lower_limit_switch"]["pin"], pull_up=True)
         if config.get("upper_limit_switch"):
@@ -25,31 +44,29 @@ class AdafruitStepperMotorHat(UsesI2C, UsesSerial, IsHomeable, HasLimits, HasPos
                 config["upper_limit_switch"]["pin"], pull_up=True
             )
 
-    async def _do_step(self, backward=False):
-        from adafruit_motor import stepper  # type: ignore
-
-        steps = self.to_steps(self._state["position"])
+    def _do_step(self, backward=False):
+        steps = self.to_usteps(self._state["position"])
         direction = stepper.BACKWARD if backward else stepper.FORWARD
-        if direction == stepper.BACKWARD and await self._get_lower_limit_switch():
+        if direction == stepper.BACKWARD and self._get_lower_limit_switch():
             return
-        elif direction == stepper.FORWARD and await self._get_upper_limit_switch():
+        elif direction == stepper.FORWARD and self._get_upper_limit_switch():
             return
-        self._stepper.onestep(direction=direction, style=stepper.MICROSTEP)
-        steps += 1 if direction == stepper.FORWARD else -1
+        self._stepper.onestep(direction=direction, style=self.style)
+        steps += self.step_size if direction == stepper.FORWARD else -self.step_size
         self._state["position"] = self.to_units(steps)
         self.logger.debug(f"{self._state['position']}")
 
     def to_units(self, usteps):
         return usteps / self.steps_per_unit / self.microsteps
 
-    def to_steps(self, units):
+    def to_usteps(self, units):
         return round(units * self.steps_per_unit * self.microsteps)
 
-    async def _get_lower_limit_switch(self):
+    def _get_lower_limit_switch(self):
         # TODO: invert
         return self._lower_pin.value
 
-    async def _get_upper_limit_switch(self):
+    def _get_upper_limit_switch(self):
         if not self._config.get("upper_limit_switch"):
             return False
         # TODO: invert
@@ -65,7 +82,7 @@ class AdafruitStepperMotorHat(UsesI2C, UsesSerial, IsHomeable, HasLimits, HasPos
             self._busy = True
             while True:
                 await asyncio.sleep(0)
-                await self._do_step(backward=True)
+                self._do_step(backward=True)
                 if self._state["position"] == prev_position:
                     break
                 prev_position = self._state["position"]
@@ -79,16 +96,16 @@ class AdafruitStepperMotorHat(UsesI2C, UsesSerial, IsHomeable, HasLimits, HasPos
     async def update_state(self):
         while True:
             async with self._lock:
-                while self.to_steps(self._state["position"]) != self.to_steps(
-                    self._state["destination"]
+                while (
+                    abs(self.to_usteps(self._state["position"] - self._state["destination"]))
+                    >= self.step_size
                 ):
                     self._busy = True
                     await asyncio.sleep(0)
-                    await self._do_step(
-                        self.to_steps(self._state["position"])
-                        > self.to_steps(self._state["destination"])
+                    self._do_step(
+                        self.to_usteps(self._state["position"])
+                        > self.to_usteps(self._state["destination"])
                     )
-
                 self._busy = False
             await self._busy_sig.wait()
 
